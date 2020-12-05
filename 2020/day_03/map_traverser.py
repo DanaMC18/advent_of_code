64d2fcd05f73cbc6b30f0f38c3ebc4a5fb48cff1
Dana Czinsky <danaczinsky@gmail.com>
2020-12-04 20:43:49 -0500

    day 03, solution 02

--- 2020/day_03/map_traverser.py
+++ 2020/day_03/map_traverser.py
@@ -2,30 +2,27 @@
 
 import os
 
+# from functools import reduce
+
 
 MAP_TXT = 'tree_map.txt'
 TREE = '#'
-
-# movements = {
-#     'right': [0, 1],
-#     'down': [1, 0]
-# }
 
 
 def get_tree_count(pattern: list):
     """Get number of trees when traversing the given map."""
     tree_matrix = _load_map()
     right_count = pattern[0]
-    # down_count = pattern[1]
+    down_count = pattern[1]
+    row = down_count
     index = 0
     tree_count = 0
 
-    for row in tree_matrix[1:]:
-        index = (index + right_count) % len(row)
-        print(row[index])
-
-        if row[index] == TREE:
+    while row < len(tree_matrix):
+        index = (index + right_count) % len(tree_matrix[row])
+        if tree_matrix[row][index] == TREE:
             tree_count += 1
+        row += down_count
 
     return tree_count
 
@@ -39,4 +36,14 @@ def _load_map():
     return raw_map.strip().split('\n')
 
 
+# patterns = [[3, 1], [1, 1], [5, 1], [7, 1], [1, 2]]
+
 # print(get_tree_count([3, 1]))
+# print(get_tree_count[1, 1])
+# print(get_tree_count[5, 1])
+# print(get_tree_count[7, 1])
+# print(get_tree_count[1, 2])
+
+# results = [get_tree_count(pattern) for pattern in patterns]
+# print(results)
+# print(reduce((lambda x, y: x * y), results))
