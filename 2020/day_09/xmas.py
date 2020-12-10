140fea02983e512ffe1dd79f46ef4bc3a7bc060d
Dana Czinsky <danaczinsky@gmail.com>
2020-12-09 23:18:38 -0500

    day 09 solution 02

--- 2020/day_09/xmas.py
+++ 2020/day_09/xmas.py
@@ -7,9 +7,8 @@ from itertools import product
 DATA_JSON = 'data.json'
 
 
-def find_corruption():
+def find_corruption(data: list):
     """Find the corrupt number in the XMAS data set."""
-    data = _load_data()
     preamble = data[:25]
 
     for num in data[25:]:
@@ -24,6 +23,27 @@ def find_corruption():
         preamble = preamble[1:]
 
 
+def find_weakness(corrupt_num: int, data: list):
+    """Find the encryption weakness in XMAS data set."""
+    curr_index = 0
+    start_index = 0
+    weak_list = list()
+
+    while start_index < len(data):
+        num = data[curr_index]
+        weak_list.append(num)
+
+        if sum(weak_list) == corrupt_num and corrupt_num not in weak_list:
+            return weak_list
+
+        if sum(weak_list) > corrupt_num:
+            start_index += 1
+            curr_index = start_index
+            weak_list = list()
+        else:
+            curr_index += 1
+
+
 def _load_data():
     """Load data json file."""
     filepath = os.path.join(os.getcwd(), os.path.dirname(__file__), DATA_JSON)
@@ -32,4 +52,10 @@ def _load_data():
 
 
 # SOLUTION 01 | 90433990
-# print(find_corruption())
+# xmas_data = _load_data()
+# corrupt_number = find_corruption(xmas_data)
+# print(corrupt_number)
+
+# SOLUTION 2 | 3289338 + 8402308 = 11691646
+# weakness_list = find_weakness(corrupt_number, xmas_data)
+# print(min(weakness_list) + max(weakness_list))
