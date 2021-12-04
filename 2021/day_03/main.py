38aa035935d356f3f08da3ac3ce2ebea4cb297a3
Dana Czinsky <danaczinsky@gmail.com>
2021-12-04 13:01:27 -0500

    day 3 part 2

--- 2021/day_03/main.py
+++ 2021/day_03/main.py
@@ -6,9 +6,10 @@ from typing import List, Tuple
 INPUT_FILE = 'input.txt'
 
 
-def part_1():
+def part_1() -> int:
     """Determine the 'power consumption'."""
-    epsilon, gamma = _get_epsilon_gamma_rates()
+    input = _load_input()
+    epsilon, gamma = _get_epsilon_gamma_rates(input)
 
     epsilon_decimal = int(epsilon, 2)
     gamma_decimal = int(gamma, 2)
@@ -16,10 +17,46 @@ def part_1():
     return epsilon_decimal * gamma_decimal
 
 
-def _get_epsilon_gamma_rates() -> Tuple[str, str]:
+def part_2() -> int:
+    """Determine the 'life support rating'."""
+    input = _load_input()
+    co2_scrubber_rating = _life_support_rate_binary(input, 0, 'epsilon')
+    oxygen_gen_rating = _life_support_rate_binary(input, 0, 'gamma')
+
+    co2_decimal = int(co2_scrubber_rating, 2)
+    oxygen_decimal = int(oxygen_gen_rating, 2)
+
+    return co2_decimal * oxygen_decimal
+
+
+def _life_support_rate_binary(input: List[str], index: int, rate_type: str) -> str:
+    """Recursively determine 'CO2 scrubber rating' or 'oxygen generator rating'.
+
+    Returns a binary num.
+
+    Args:
+      input (list): list of binary numbers
+      index (int): position at which to check a bit value of binary number
+      rate_type (str): either 'epsilon' or 'gamma'
+        - if 'epsilon', get 'CO2 scrubber rating'
+        - if 'gamma', get 'oxygen generator rating'
+    """
+    if len(input) == 1:
+        return input[0]
+
+    epsilon, gamma = _get_epsilon_gamma_rates(input)
+
+    bit = gamma[index] if rate_type == 'gamma' else epsilon[index]
+
+    new_input = [num for num in input if num[index] == bit]
+    index += 1
+
+    return _life_support_rate_binary(new_input, index, rate_type)
+
+
+def _get_epsilon_gamma_rates(input: List[str]) -> Tuple[str, str]:
     """Determine epsilon (min) and gamma (max) rates from input."""
     epsilon = gamma = ''
-    input = _load_input()
     size = len(input[0])
 
     for i in range(size):
@@ -46,4 +83,5 @@ def _load_input() -> List[str]:
     return data.strip().split('\n')
 
 
-print(part_1())   # 4160394
+# print(part_1())   # 4160394
+# print(part_2())     # 4125600
