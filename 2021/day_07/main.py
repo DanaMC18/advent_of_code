2c2af67ad488377e8f9b10d645313b9cdc7e930e
Dana Czinsky <danaczinsky@gmail.com>
2021-12-07 23:37:56 -0500

    day 7 part 2 but with math

--- 2021/day_07/main.py
+++ 2021/day_07/main.py
@@ -44,3 +44,41 @@ def _load_input() -> List[int]:
 
 # print(part_1())   # 336701
 # print(part_2())   # 95167302
+
+
+# NOTES:
+# Apparently the most efficient position in part 1 is the median of the list.
+# For part 2, the most effefient position is the mean of the list.
+# Part 2 also makes use of triangular numbers: n * (n + 1) / 2
+# All the work done above to find the right position isn't necessary or effecient.
+
+# PART 1:
+def part_1_median() -> int:
+    """Use median to find fuel total."""
+    input = _load_input()
+    length = len(input)
+    sorted_input = sorted(input)
+    median = sorted_input[length // 2]
+
+    return sum([abs(x - median) for x in input])
+
+
+# print(part_1_median())
+
+
+# PART 2:
+def part_2_mean() -> int:
+    """Use mean and triangular numbers to find fuel total."""
+    input = _load_input()
+    length = len(input)
+    mean = sum(input) // length
+
+    return sum(_triangulate_fuel(abs(x - mean)) for x in input)
+
+
+def _triangulate_fuel(pos: int) -> int:
+    """Use trianguluar numbers to get fuel from position."""
+    return pos * (pos + 1) // 2
+
+
+# print(part_2_mean())
