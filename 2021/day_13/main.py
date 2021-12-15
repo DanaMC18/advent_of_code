b6eb3493a53e003ac8adbefbbea9a034f4dbc15c
Dana Czinsky <danaczinsky@gmail.com>
2021-12-14 19:16:31 -0500

    day 13 part 1

---
+++ 2021/day_13/main.py
@@ -0,0 +1,137 @@
+"""Main solution file: day 13."""
+
+import os
+from typing import Dict, List, Tuple
+
+INPUT_FILE = 'input.txt'
+
+
+def part_1() -> int:
+    """Return number of dots after folding graph once."""
+    input, folds = _load_input()
+
+    rows = max([coord[1] for coord in input])
+    cols = max([coord[0] for coord in input])
+
+    blank_line = ['.' for _ in range(cols + 1)]
+    blank_graph = [blank_line.copy() for _ in range(rows + 1)]
+
+    clean_coords = list()
+
+    for coord in input:
+        col = coord[0]
+        row = coord[1]
+        blank_graph[row][col] = '#'
+        clean_coords.append([row, col])
+
+    folded_coords = clean_coords.copy()
+    folded_graph = blank_graph.copy()
+
+    for fold in folds[:1]:
+        x = fold.get('x')
+        y = fold.get('y')
+
+        if x:
+            new_coords, new_graph = _fold_left(folded_coords, folded_graph, x)
+
+            folded_coords = new_coords
+            folded_graph = [line[:x] for line in new_graph]
+
+        if y:
+            new_coords, new_graph = _fold_up(folded_coords, folded_graph, y)
+
+            folded_coords = new_coords
+            folded_graph = new_graph[:y]
+
+    return sum([line.count('#') for line in folded_graph])
+
+
+def _fold_left(
+    coords: List[List[int]],
+    graph: List[List[str]],
+    x: int
+):
+    """Fold in half along x axis.
+
+    Args:
+        coords (list): current list coordinates
+        graph (matrix): current iteration of graph
+        x (int): the col on which to fold the graph
+
+    Returns:
+        new_coords (list): new coordinates after fold
+        new_graph (matrix): updated/marked graph after fold
+    """
+    rows = len(graph)
+    cols = len(graph[0])
+
+    new_coords = list()
+    new_graph = graph.copy()
+
+    for coord in coords:
+        row, col = coord
+        new_col = (cols - col - 1) if col > x else col
+
+        if 0 <= row < rows:
+            new_coords.append([row, new_col])
+            new_graph[row][new_col] = '#'
+
+    return new_coords, new_graph
+
+
+def _fold_up(
+    coords: List[List[int]],
+    graph: List[List[str]],
+    y: int
+) -> List[List[str]]:
+    """Fold in half along the y axis.
+
+    Args:
+        coords (list): current list coordinates
+        graph (matrix): current iteration of graph
+        y (int): the row on which to fold the graph
+
+    Returns:
+        new_coords (list): new coordinates after fold
+        new_graph (matrix): updated/marked graph after fold
+    """
+    rows = len(graph)
+    cols = len(graph[0])
+
+    new_coords = list()
+    new_graph = graph.copy()
+
+    for coord in coords:
+        row, col = coord
+        new_row = (rows - row - 1) if row > y else row
+
+        if 0 <= col < cols:
+            new_coords.append([new_row, col])
+            new_graph[new_row][col] = '#'
+
+    return new_coords, new_graph
+
+
+def _load_input() -> Tuple[List[List[int]], List[Dict[str, int]]]:
+    """Load input data from text file."""
+    filepath = os.path.join(os.getcwd(), os.path.dirname(__file__), INPUT_FILE)
+    f = open(filepath, 'r')
+    data = f.read()
+    f.close()
+
+    raw_coords, raw_folds = data.strip().split('\n\n')
+
+    coords = [raw_coord.split(',') for raw_coord in raw_coords.split('\n')]
+    input = [[int(c) for c in coord] for coord in coords]
+
+    folds = raw_folds.split('\n')
+    fold_dicts = list()
+
+    for f in folds:
+        key, value = f.split('fold along ')[1].split('=')
+        fold_dicts.append({key: int(value)})
+
+    return input, fold_dicts
+
+
+# print(part_1())   # 814
