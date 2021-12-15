dd49c9a6ace72c618f9c3fa8a3f1d08188577ef1
Dana Czinsky <danaczinsky@gmail.com>
2021-12-15 00:12:35 -0500

    day 14 part 2; refactor part 1

--- 2021/day_14/main.py
+++ 2021/day_14/main.py
@@ -7,49 +7,63 @@ from typing import Dict, Tuple
 INPUT_FILE = 'input.txt'
 
 
-def part_1() -> int:
-    """Return diff of most and least common polymer after 10 steps."""
-    initial_template, rules = _load_input()
-    template = _execute_steps(rules, 10, 1, initial_template)
+def main(step_count: int) -> int:
+    """Return diff of most and least common polymer after N steps."""
+    template, rules = _load_input()
+    pair_counts = Counter()
 
-    char_counts = Counter(template).most_common()
-    max_qty = char_counts[0][1]
-    min_qty = char_counts[-1][1]
+    for start in range(len(template)):
+        end = start + 2
+        key = template[start:end]
+        if len(key) == 2:
+            pair_counts[key] += 1
 
-    return max_qty - min_qty
+    new_pair_counts = _execute_steps(
+        pair_counts,
+        rules,
+        step_count,
+        1
+    )
+
+    char_counts = Counter(template[0])
+
+    for pair, count in new_pair_counts.items():
+        char = pair[1]
+        char_counts[char] += count
+
+    return max(char_counts.values()) - min(char_counts.values())
 
 
 def _execute_steps(
-    rules: dict,
+    pair_counts: Dict[str, int],
+    rules: Dict[str, str],
     step_count: int,
-    step: int,
-    template: str
-) -> str:
-    """Recursively execute N steps of polymer insertion. Return new template.
+    step: int
+):
+    """Recursively count pair of chars after N steps of polymer insertion.
 
     Args:
-        rules (dict): insertion rules (ex: {'AB' -> 'C'})
+        pair_counts (dict): map of pair counts (ex: {'AX': 10})
+        rules (dict): insertion rules (ex: {'AB': 'X'})
         step_count (int): number of steps to execute
         step (int): number of current step
-        template (str): string on which to execute step
+
+    Returns:
+        pair_counts (dict): updated map of pair counts
     """
     if step > step_count:
-        return template
+        return pair_counts
 
-    new_template = ''
-    for start in range(len(template)):
-        end = start + 2
-        section = template[start:end]
-        mid = rules.get(section)
+    pair_counts_copy = pair_counts.copy()
+    new_pair_counts = Counter()
 
-        if start == 0:
-            new_template += template[start]
+    for k in pair_counts_copy.keys():
+        ax = k[0] + rules[k]
+        xb = rules[k] + k[1]
+        new_pair_counts[ax] += pair_counts_copy[k]
+        new_pair_counts[xb] += pair_counts_copy[k]
 
-        if mid:
-            new_section = f'{mid}{template[start + 1]}'
-            new_template += new_section
-
-    return _execute_steps(rules, step_count, step + 1, new_template)
+    return _execute_steps(new_pair_counts, rules, step_count, step + 1)
 
 
 def _load_input() -> Tuple[str, Dict[str, str]]:
@@ -70,4 +84,5 @@ def _load_input() -> Tuple[str, Dict[str, str]]:
     return template, rule_dict
 
 
-print(part_1())   # 2170
+# print(main(10))   # 2170
+# print(main(40))   # 2422444761283
