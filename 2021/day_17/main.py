333be785f97b404a7923ce5f3f640dfc0f022951
Dana Czinsky <danaczinsky@gmail.com>
2021-12-20 22:37:36 -0500

    day 17 part 2

--- 2021/day_17/main.py
+++ 2021/day_17/main.py
@@ -5,18 +5,67 @@ from typing import Dict, List
 
 INPUT_FILE = 'input.txt'
 
-# abs(y) * (abs(y) - 1) / 2
 
-
-def part_1():
+def part_1() -> int:
     """Return highest y position to hit target area with highest arc."""
     input = _load_input()
     y = input.get('y')
 
-    # triangulate
+    # triangulate forumula using abs in case of negatives
     min_y = min(y)
-    highest_y = abs(min_y) * (abs(min_y) - 1) / 2
-    return int(highest_y)
+    highest_y = abs(min_y) * (abs(min_y) - 1) // 2
+    return highest_y
+
+
+def part_2() -> int:
+    """Return number of distinct initital velocity values that hit target area."""
+    input = _load_input()
+    x_range = input.get('x')
+    y_range = input.get('y')
+
+    end_x = x_range[1] + 1
+    start_y = -abs(y_range[0])
+    end_y = abs(y_range[0])
+
+    valid_velocity_count = 0
+
+    for x_val in range(1, end_x):
+        for y_val in range(start_y, end_y):
+            if _is_valid(x_range, y_range, x_val, y_val):
+                valid_velocity_count += 1
+
+    return valid_velocity_count
+
+
+def _is_valid(
+    x_range: List[int],
+    y_range: List[int],
+    x_val: int,
+    y_val: int
+) -> bool:
+    """Return if points will fall in given target ranges.
+
+    Args:
+        x_range (list): target range on x-axis
+        y_range (list): target range on y-axis
+        x_val (int): point on x-axis
+        y_val (int): point on y-axis
+    """
+    x = y = 0
+    x1, x2 = x_range
+    y1, y2 = y_range
+
+    while y > -abs(y1):
+        x += x_val
+        y += y_val
+
+        x_val = x_val - 1 if x_val > 0 else x_val
+        y_val -= 1
+
+        if x1 <= x <= x2 and -abs(y1) <= y <= -abs(y2):
+            return True
+
+    return False
 
 
 def _load_input() -> Dict[str, List[int]]:
@@ -38,3 +87,4 @@ def _load_input() -> Dict[str, List[int]]:
 
 
 # print(part_1())   # 13203
+# print(part_2())   # 5644
