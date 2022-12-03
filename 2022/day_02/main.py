98ae3dc87e84a747f9d07b6345f9437adafcaf35
Dana Czinsky <danaczinsky@gmail.com>
2022-12-02 22:41:00 -0500

    day 2 part 2

--- 2022/day_02/main.py
+++ 2022/day_02/main.py
@@ -4,10 +4,15 @@ import os
 from typing import List
 
 INPUT_FILE = 'input.txt'
-STRAT_MAP = {
+PART_1_STRAT = {
     'X': {'A': 4, 'B': 1, 'C': 7},
     'Y': {'A': 8, 'B': 5, 'C': 2},
     'Z': {'A': 3, 'B': 9, 'C': 6}
+}
+PART_2_STRAT = {
+    'A': {'X': 3, 'Y': 4, 'Z': 8},
+    'B': {'X': 1, 'Y': 5, 'Z': 9},
+    'C': {'X': 2, 'Y': 6, 'Z': 7}
 }
 
 
@@ -18,9 +23,22 @@ def part_1() -> int:
 
     for round in input:
         opponent, player = round.split(' ')
-        score += STRAT_MAP[player][opponent]
+        score += PART_1_STRAT[player][opponent]
 
     return score
+
+
+def part_2() -> int:
+    """Get total score according to strategy guide."""
+    input = _load_data()
+    score = 0
+
+    for round in input:
+        opponent, result = round.split(' ')
+        score += PART_2_STRAT[opponent][result]
+
+    return score
+
 
 # # # # # # # #
 # LOAD INPUT  #
@@ -36,4 +54,5 @@ def _load_data() -> List[str]:
     return data.strip().split('\n')
 
 
-print(part_1())  # 15523
+# print(part_1())  # 15523
+# print(part_2())  # 15702
