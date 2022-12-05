b5cd70fd4eab9398cdcfe057076e50a8cb9ee816
Dana Czinsky <danaczinsky@gmail.com>
2022-12-04 21:05:25 -0500

    day 4 part 1

---
+++ 2022/day_04/main.py
@@ -0,0 +1,49 @@
+"""Solution file: day 04."""
+
+import os
+from typing import List
+
+INPUT_FILE = 'input.txt'
+
+
+def part_1() -> int:
+    """Find number of assignements that fully overlap."""
+    input = _load_data()
+    overlaps = 0
+
+    for pair in input:
+        sect_1, sect_2 = pair[0], pair[1]
+
+        sect_1_min, sect_1_max = [int(i) for i in sect_1.split('-')]
+        sect_2_min, sect_2_max = [int(i) for i in sect_2.split('-')]
+
+        section_1 = list(range(sect_1_min, sect_1_max + 1))
+        section_2 = list(range(sect_2_min, sect_2_max + 1))
+
+        dupes = set(section_1).intersection(section_2)
+
+        if len(dupes) == len(section_1) or len(dupes) == len(section_2):
+            overlaps += 1
+
+    return overlaps
+
+
+# # # # # # # #
+# LOAD INPUT  #
+# # # # # # # #
+
+def _load_data() -> List[List]:
+    """Load data from txt file. Return list."""
+    filepath = os.path.join(os.getcwd(), os.path.dirname(__file__), INPUT_FILE)
+    f = open(filepath, 'r')
+    data = f.read()
+
+    input = data.strip().split('\n')
+    pairs = [[i for i in pair.split(',')] for pair in input]
+    # test = [[[int(i) for i in sect.split('-')] for sect in pair] for pair in pairs]
+    # # return [item for sublist in test for item in sublist]
+    # return test
+    return pairs
+
+
+print(part_1())  # 562
