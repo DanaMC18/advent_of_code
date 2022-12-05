410db3e791dede11f5a9851cdf46a9444c0c99a7
Dana Czinsky <danaczinsky@gmail.com>
2022-12-04 21:13:12 -0500

    day 4 part 2

--- 2022/day_04/main.py
+++ 2022/day_04/main.py
@@ -7,7 +7,7 @@ INPUT_FILE = 'input.txt'
 
 
 def part_1() -> int:
-    """Find number of assignements that fully overlap."""
+    """Find number of assignments that fully overlap."""
     input = _load_data()
     overlaps = 0
 
@@ -28,6 +28,28 @@ def part_1() -> int:
     return overlaps
 
 
+def part_2() -> int:
+    """Find number of assignments that partially overlap."""
+    input = _load_data()
+    overlaps = 0
+
+    for pair in input:
+        sect_1, sect_2 = pair[0], pair[1]
+
+        sect_1_min, sect_1_max = [int(i) for i in sect_1.split('-')]
+        sect_2_min, sect_2_max = [int(i) for i in sect_2.split('-')]
+
+        section_1 = list(range(sect_1_min, sect_1_max + 1))
+        section_2 = list(range(sect_2_min, sect_2_max + 1))
+
+        dupes = set(section_1).intersection(section_2)
+
+        if dupes:
+            overlaps += 1
+
+    return overlaps
+
+
 # # # # # # # #
 # LOAD INPUT  #
 # # # # # # # #
@@ -40,10 +62,8 @@ def _load_data() -> List[List]:
 
     input = data.strip().split('\n')
     pairs = [[i for i in pair.split(',')] for pair in input]
-    # test = [[[int(i) for i in sect.split('-')] for sect in pair] for pair in pairs]
-    # # return [item for sublist in test for item in sublist]
-    # return test
     return pairs
 
 
-print(part_1())  # 562
+# print(part_1())  # 562
+# print(part_2())  # 924
