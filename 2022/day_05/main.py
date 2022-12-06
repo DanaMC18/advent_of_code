b3d52c0fd0e0cd8295cfaefec0f0b88e16f669ba
Dana Czinsky <danaczinsky@gmail.com>
2022-12-05 22:16:42 -0500

    day 5 part 2

--- 2022/day_05/main.py
+++ 2022/day_05/main.py
@@ -16,7 +16,7 @@ def part_1() -> str:
         source_stack = new_stacks[source]
         dest_stack = new_stacks[dest]
 
-        for i in range(crate_num):
+        for _ in range(crate_num):
             crate = source_stack.pop()
             dest_stack.append(crate)
 
@@ -24,4 +24,29 @@ def part_1() -> str:
     return ''.join(top_crates)
 
 
-print(part_1())  # HNSNMTLHQ
+def part_2() -> str:
+    """Get crates on top of each stack after rearranging, but different."""
+    new_stacks = deepcopy(STACKS)
+
+    for proc in PROCEDURE:
+        crate_num = proc['move']
+        source = proc['from'] - 1
+        dest = proc['to'] - 1
+
+        source_stack = new_stacks[source]
+        dest_stack = new_stacks[dest]
+        crates = []
+
+        for _ in range(crate_num):
+            crate = source_stack.pop()
+            crates.append(crate)
+
+        crates.reverse()
+        dest_stack.extend(crates)
+
+    top_crates = [s[-1] for s in new_stacks if s]
+    return ''.join(top_crates)
+
+
+# print(part_1())  # HNSNMTLHQ
+# print(part_2())  # RNLFDJMCT
