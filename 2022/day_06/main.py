122b33b903f33da34865e57ba2c507566d2f73df
Dana Czinsky <danaczinsky@gmail.com>
2022-12-06 21:22:03 -0500

    day 6 part 2

--- 2022/day_06/main.py
+++ 2022/day_06/main.py
@@ -3,20 +3,25 @@
 from input import INPUT
 
 
-def part_1() -> int:
-    """Return position of start-of-packet marker."""
+def main(packet_size: int) -> int:
+    """Return position of start-of-packet marker.
+
+    Args:
+        packet_size (int): size of the packet to look for
+    """
     marker = ''
 
     for i in range(len(INPUT)):
         marker = marker + INPUT[i]
 
-        if len(marker) < 4:
+        if len(marker) < packet_size:
             continue
 
-        if len(set(marker)) == 4:
+        if len(set(marker)) == packet_size:
             return i + 1
 
         marker = marker[1:]
 
 
-# print(part_1())  # 1080
+# print(main(4))  # 1080
+# print(main(14))  # 3645
