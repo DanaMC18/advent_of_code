ced78cf6879c139fd5e97b5449c16b99f5de7a51
Dana Czinsky <danaczinsky@gmail.com>
2023-12-01 19:27:18 -0500

    day 1 part 2

--- 2023/day_01/main.py
+++ 2023/day_01/main.py
@@ -4,6 +4,7 @@ import os
 from typing import List
 
 INPUT_FILE = 'input.txt'
+NUMBERS_AS_WORDS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']  # noqa
 
 
 def part_one() -> int:
@@ -14,6 +15,38 @@ def part_one() -> int:
     for line in calibration_data:
         numbers = [char for char in line if char.isnumeric()]
         value = f'{numbers[0]}{numbers[-1]}'
+        calibration_values.append(value)
+
+    return sum([int(num) for num in calibration_values])
+
+
+def part_two() -> int:
+    """Return the sum of all calibration values in input."""
+    calibration_data = _load_data()
+    calibration_values = []
+    filtered_calibration_data = []
+
+    for line in calibration_data:
+        substring = ''
+        numbers = ''
+
+        for char in line:
+            substring = substring + char
+
+            if char.isnumeric():
+                numbers = numbers + char
+                continue
+
+            for num in NUMBERS_AS_WORDS:
+                if num in substring:
+                    val = NUMBERS_AS_WORDS.index(num)
+                    numbers = numbers + str(val)
+                    substring = substring[-1]
+
+        filtered_calibration_data.append(numbers)
+
+    for line in filtered_calibration_data:
+        value = f'{line[0]}{line[-1]}'
         calibration_values.append(value)
 
     return sum([int(num) for num in calibration_values])
@@ -33,4 +66,5 @@ def _load_data() -> List[str]:
     return data.strip().split('\n')
 
 
-print(part_one()) # 55971
+# print(part_one())   # 55971
+# print(part_two()) # 54719
