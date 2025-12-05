33983b7f61a17d9032996d38923e9645e2ef414d
Dana Czinsky <danaczinsky@gmail.com>
2025-12-04 23:23:10 -0500

    2025 day 2

--- 2025/day_02/main.py
+++ 2025/day_02/main.py
@@ -16,24 +16,48 @@ def part_one() -> int:
         end = int(end)
 
         for id in range(start, end + 1):
-            if not _is_valid(str(id)):
+            if not _is_valid_part_one(str(id)):
                 invalid_ids.append(id)
 
     return sum(invalid_ids)
 
 
-# TODO: logic here is wrong -- need to account for twice-repeated sequences, which this isn't doing
-def _is_valid(id: str) -> bool:
+def _is_valid_part_one(id: str) -> bool:
     """Return whether ID is valid or not"""
-    char_map = {}
+    if len(id) % 2 != 0:
+        return True
 
-    for char in id:
-        if char in char_map:
-            char_map[char] += 1
-        else:
-            char_map[char] = 1
+    middle_index = len(id) // 2
+    return id[:middle_index] != id[middle_index:]
 
-    return len(set(char_map.values())) > 1
+
+def part_two() -> int:
+    """Return the sum of all invalid IDs."""
+    data = _load_data()
+    invalid_ids = []
+
+    for id_range in data:
+        start, end = id_range.split('-')
+        start = int(start)
+        end = int(end)
+
+        for id in range(start, end + 1):
+            if not _is_valid_part_two(str(id)):
+                invalid_ids.append(id)
+
+    return sum(invalid_ids)
+
+
+def _is_valid_part_two(id: str) -> bool:
+    """Return whether ID is valid or not"""
+    id_len = len(id)
+
+    for idx in range(id_len // 2):
+        quotient, remainder = divmod(id_len, idx + 1)
+        if remainder == 0 and id[:idx + 1] * quotient == id:
+            return False
+
+    return True
 
 
 # # # # # # # #
@@ -49,4 +73,5 @@ def _load_data() -> List[str]:
 
     return data.strip().split(',')
 
-print(part_one())
+# print(part_one()) # 15873079081
+# print(part_two()) # 22617871034
